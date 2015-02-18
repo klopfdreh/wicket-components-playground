ca1737221cef1af05cbab929576c42972b8bbac4
klopfdreh <klopfdreh@192.168.2.110>
2015-02-18 16:58:28 +0100

    PackageResourceStream > PackageResourceReference

--- wicket-components-playground/src/main/java/org/apache/wicket/markup/html/image/CanvasImage.java
+++ wicket-components-playground/src/main/java/org/apache/wicket/markup/html/image/CanvasImage.java
@@ -3,11 +3,12 @@ package org.apache.wicket.markup.html.image;
 import java.io.DataInputStream;
 
 import org.apache.wicket.WicketRuntimeException;
-import org.apache.wicket.core.util.resource.PackageResourceStream;
 import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.WebComponent;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.request.resource.PackageResourceReference;
 import org.apache.wicket.util.crypt.Base64;
+import org.apache.wicket.util.resource.IResourceStream;
 
 /**
  * The canvas image is used to embed the complete image content within a HTML document.
@@ -15,11 +16,12 @@ import org.apache.wicket.util.crypt.Base64;
  * @author Tobias Soloschenko
  * 
  */
-public class CanvasImage extends WebComponent {
+public class CanvasImage extends WebComponent
+{
 
 	private static final long serialVersionUID = 1L;
 
-	private PackageResourceStream packageResourceStream;
+	private PackageResourceReference packageResourceReference;
 
 	/**
 	 * Creates an canvas image
@@ -29,8 +31,9 @@ public class CanvasImage extends WebComponent {
 	 * @param packageResourceStream
 	 *            the package resource stream of the image
 	 */
-	public CanvasImage(String id, PackageResourceStream packageResourceStream) {
-		this(id, null, packageResourceStream);
+	public CanvasImage(String id, PackageResourceReference packageResourceReference)
+	{
+		this(id, null, packageResourceReference);
 	}
 
 	/**
@@ -43,34 +46,46 @@ public class CanvasImage extends WebComponent {
 	 * @param packageResourceStream
 	 *            the package resource stream of the image
 	 */
-	public CanvasImage(String id, IModel<?> model, PackageResourceStream packageResourceStream) {
+	public CanvasImage(String id, IModel<?> model, PackageResourceReference packageResourceReference)
+	{
 		super(id, model);
-		this.packageResourceStream = packageResourceStream;
+		this.packageResourceReference = packageResourceReference;
 	}
 
 	/**
 	 * Renders the complete image tag with the base64 encoded content.
 	 */
 	@Override
-	protected void onComponentTag(ComponentTag tag) {
+	protected void onComponentTag(ComponentTag tag)
+	{
 		checkComponentTag(tag, "img");
 		super.onComponentTag(tag);
 
-		if (packageResourceStream != null) {
-			try {
+		IResourceStream packageResourceStream = packageResourceReference.getResource()
+			.getResourceStream();
+		if (packageResourceStream != null)
+		{
+			try
+			{
 				StringBuilder builder = new StringBuilder();
 				builder.append("data:");
 				builder.append(packageResourceStream.getContentType());
 				builder.append(";base64,");
-				byte[] bytes = new byte[(int) packageResourceStream.length().bytes()];
-				DataInputStream dataInputStream = new DataInputStream(packageResourceStream.getInputStream());
+				byte[] bytes = new byte[(int)packageResourceStream.length().bytes()];
+				DataInputStream dataInputStream = new DataInputStream(
+					packageResourceStream.getInputStream());
 				dataInputStream.readFully(bytes);
 				builder.append(Base64.encodeBase64String(bytes));
 				tag.put("src", builder.toString());
-			} catch (Exception e) {
-				throw new WicketRuntimeException("An error occured while reading the package resource stream", e);
 			}
-		} else {
+			catch (Exception e)
+			{
+				throw new WicketRuntimeException(
+					"An error occured while reading the package resource stream", e);
+			}
+		}
+		else
+		{
 			// If the package resource stream is not set create an empty image
 			tag.put("src", "#");
 			tag.put("style", "display:none;");
